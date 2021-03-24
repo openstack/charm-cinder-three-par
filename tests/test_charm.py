dc222bd88a70110c6df997a88e619c391bedb4a0
Pedro Guimaraes <pedro.guimaraes@canonical.com>
2021-03-24 10:29:54 +0100

    breaked TEST_3PAR_CINDER_BACKEND string

--- tests/test_charm.py
+++ tests/test_charm.py
@@ -8,7 +8,20 @@ import ops.model
 import ops.testing
 from charm import CharmCinderThreeParCharm
 
-TEST_3PAR_CINDER_BACKEND = '{"cinder": {"/etc/cinder/cinder.conf": {"sections": {"charm-cinder-three-par": [["san_ip", "1.2.3.4"], ["san_login", "login"], ["san_password", "pwd"], ["hpe3par_username", ""], ["hpe3par_password", ""], ["hpe3par_api_url", "test.url"], ["hpe3par_cpg", ""], ["volume-backend-name", "charm-cinder-three-par"], ["volume_driver", "cinder.volume.drivers.hpe.hpe_3par_fc.HPE3PARFCDriver"]]}}}}'
+TEST_3PAR_CINDER_BACKEND = '\
+{"cinder": \
+{"/etc/cinder/cinder.conf": \
+{"sections": \
+{"charm-cinder-three-par": \
+[["san_ip", "1.2.3.4"], \
+["san_login", "login"], \
+["san_password", "pwd"], \
+["hpe3par_username", ""], \
+["hpe3par_password", ""], \
+["hpe3par_api_url", "test.url"], \
+["hpe3par_cpg", ""], \
+["volume-backend-name", "charm-cinder-three-par"], \
+["volume_driver", "cinder.volume.drivers.hpe.hpe_3par_fc.HPE3PARFCDriver"]]}}}}'
 
 class TestCharm(unittest.TestCase):
     def setUp(self):
@@ -27,7 +40,6 @@ class TestCharm(unittest.TestCase):
           "hpe3par-api-url": "test.url"
         })
         rel = self.model.get_relation('storage-backend', 0)
-        print(rel.data[self.model.unit])
         self.assertIsInstance(rel, ops.model.Relation)
         self.assertEqual(rel.data[self.model.unit], {'backend_name': 'charm-cinder-three-par', 'subordinate_configuration': TEST_3PAR_CINDER_BACKEND})
 
