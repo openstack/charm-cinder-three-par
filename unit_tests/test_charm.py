99a38e27bcf6402e0fc623de5d3319a64da86627
Bartosz Woronicz <bartosz.woronicz@canonical.com>
2021-04-07 14:35:10 +0200

    fix tests to reflect config

--- unit_tests/test_charm.py
+++ unit_tests/test_charm.py
@@ -12,13 +12,21 @@ TEST_3PAR_CONFIG = '\
 {"/etc/cinder/cinder.conf": \
 {"sections": \
 {"charm-cinder-three-par": \
-[["san_ip", "1.2.3.4"], \
+[["debug", false], \
+["driver_type", "fc"], \
+["hpe3par_iscsi_ips", ""], \
+["hpe3par_iscsi_chap_enabled", true], \
+["hpe3par_snapshot_expiration", 48], \
+["hpe3par_snapshot_retention", 72], \
+["san_ip", "1.2.3.4"], \
 ["san_login", "login"], \
 ["san_password", "pwd"], \
 ["hpe3par_username", ""], \
 ["hpe3par_password", ""], \
 ["hpe3par_api_url", "test.url"], \
 ["hpe3par_cpg", ""], \
+["hpe3par_cpg_snap", ""], \
+["hpe3par_target_nsp", ""], \
 ["volume-backend-name", "charm-cinder-three-par"], \
 ["volume_driver", \
 "cinder.volume.drivers.hpe.hpe_3par_fc.HPE3PARFCDriver"]]}}}}'
